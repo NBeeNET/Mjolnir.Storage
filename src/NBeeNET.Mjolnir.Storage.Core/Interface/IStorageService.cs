cfc7810c86676b3555436665a33cee3497888463
liuxiangyu <liuxyxh@outlook.com>
2019-04-30 17:50:30 +0800

    StorageService

--- src/NBeeNET.Mjolnir.Storage.Core/Interface/IStorageService.cs
+++ src/NBeeNET.Mjolnir.Storage.Core/Interface/IStorageService.cs
@@ -6,38 +6,38 @@ using System.Text;
 namespace NBeeNET.Mjolnir.Storage.Core.Interface
 {
     /// <summary>
-    /// 本地存储服务接口
+    /// 存储服务接口
     /// </summary>
-    public interface ILocalStorageService
+    public interface IStorageService
     {
         /// <summary>
-        /// 保存文件
+        /// 复制文件夹
         /// </summary>
-        /// <param name="fileJson"></param>
-        /// <param name="fileStream"></param>
+        /// <param name="sourceDir"></param>
+        /// <param name="saveDir"></param>
         /// <returns></returns>
-        bool SaveFile(object fileJson, Stream fileStream);
+        bool CopyDirectory(string sourceDir, string destinationDir, bool isOverwriteExisting);
 
         /// <summary>
         /// 获取文件信息
         /// </summary>
         /// <param name="fileGuid"></param>
         /// <returns></returns>
-        object GetFileInfo(string fileGuid);
+        T GetInfo<T>(string guid);
 
         /// <summary>
         /// 获取文件的存储路径
         /// </summary>
         /// <param name="fileGuid"></param>
         /// <returns></returns>
-        string GetFilePath(string fileGuid);
+        string GetPath(string guid);
 
         /// <summary>
         /// 删除文件
         /// </summary>
         /// <param name="fileGuid"></param>
         /// <returns></returns>
-        bool DeleteFile(string fileGuid);
+        bool Delete(string guid);
 
     }
 }
