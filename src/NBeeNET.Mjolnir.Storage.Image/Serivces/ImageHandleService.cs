6ed9994ef0927c14df3d1d192eda3f1a9f4faff4
liuxiangyu <liuxyxh@outlook.com>
2019-05-05 18:40:34 +0800

    提交本地

--- src/NBeeNET.Mjolnir.Storage.Image/Serivces/ImageHandleService.cs
+++ src/NBeeNET.Mjolnir.Storage.Image/Serivces/ImageHandleService.cs
@@ -17,85 +17,39 @@ namespace NBeeNET.Mjolnir.Storage.Image.Serivces
     /// </summary>
     public class ImageHandleService
     {
-        public IFormFile formFile = null;
+      
 
         public ImageHandleService()
         {
 
         }
 
-        public ImageHandleService(IFormFile file)
-        {
-            formFile = file;
-        }
-
 
         /// <summary>
-        /// 创建临时文件
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        public async Task<string> CreateTempFile(string id)
-        {
-            return await new Core.TempFileOperation().WriteTempFile(formFile, id);
-        }
-
-        /// <summary>
-        /// 创建Json文件
-        /// </summary>
-        /// <param name="imageRequest"></param>
-        /// <returns></returns>
-        public async Task CreateJsonFile(ImageRequest imageRequest)
-        {
-            FileStrorageModel model = new FileStrorageModel();
-            model.Id = imageRequest.Id;
-            model.Name = imageRequest.Name;
-            model.PathUrl = imageRequest.Path;
-            model.StartTime = DateTime.MinValue;
-            List<FileStrorageTaskModel> tasks = new List<FileStrorageTaskModel>();
-            tasks.Add(new FileStrorageTaskModel() { Name = "MakeThumbnail", Param = "{ width:100; height:100;mode:'W'}", Status = "" });
-            model.Tasks = tasks;
-            string JsonStr = JsonConvert.SerializeObject(model);
-            new Core.TempFileOperation().WriteJsonFile(model.Id, JsonStr);
-        }
-
-        /// <summary>
-        /// 保存图片
+        /// 处理图片
         /// </summary>
         /// <param name="tempFilePath"></param>
         /// <returns></returns>
-        public async Task<bool> SaveImage(string tempFilePath)
+        public async Task<bool> Processing(IFormFile file, string id)
         {
             //写入临时文件
-            if (tempFilePath != "")
-            {
-                var tempFile = new FileInfo(tempFilePath);
-                var id = tempFile.Name.Replace(tempFile.Extension, "");
 
-                string sourceDir = new FileInfo(tempFilePath).DirectoryName;
+            //写入Json文件
 
-                string rootDirectory = Directory.GetCurrentDirectory() + "\\wwwroot\\NBeeNET\\";
-                if (!Directory.Exists(rootDirectory))
-                {
-                    Directory.CreateDirectory(rootDirectory);
-                }
-                string destinationDir = rootDirectory + "\\Images\\" + id + "\\";
-                if (!Directory.Exists(destinationDir))
-                {
-                    Directory.CreateDirectory(destinationDir);
-                }
-                //复制到目录
-                IStorageService storageService = new LocalStorageService();
-                return await storageService.CopyDirectory(sourceDir, destinationDir, true);
+            //处理任务
 
-            }
+            //更新Json文件
+
+            //删除临时文件
             return false;
         }
 
+
+       
         /// <summary>
         /// 文件处理队列
         /// </summary>
-        private Queue<FileStrorageTaskModel> _ProcessingQueue = new Queue<FileStrorageTaskModel>();
+        private Queue<T> _ProcessingQueue = new Queue<T>();
 
         /// <summary>
         /// 处理图片
