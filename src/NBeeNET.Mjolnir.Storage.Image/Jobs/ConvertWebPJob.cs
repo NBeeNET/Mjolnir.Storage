63cc4cd366d43e2858a3ad6197c4819ca85570ed
liuxiangyu <liuxyxh@outlook.com>
2019-05-07 15:22:13 +0800

    WebP格式转换

--- src/NBeeNET.Mjolnir.Storage.Image/Jobs/ConvertWebPJob.cs
+++ src/NBeeNET.Mjolnir.Storage.Image/Jobs/ConvertWebPJob.cs
@@ -15,7 +15,7 @@ namespace NBeeNET.Mjolnir.Storage.Image.Jobs
             FileInfo fileInfo = new FileInfo(tempFilePath);
             var fileName = fileInfo.Name.Replace(fileInfo.Extension, "");
 
-            string webPName = string.Format("{0}_{1}", fileName, ".webP");
+            string webPName = string.Format("{0}{1}", fileName, ".webP");
             string webPPath = fileInfo.DirectoryName + "\\" + webPName;
 
 
@@ -24,7 +24,7 @@ namespace NBeeNET.Mjolnir.Storage.Image.Jobs
             job.Value = Core.StorageOperation.GetUrl(webPName);
             job.CreateTime = DateTime.Now;
 
-            Imazen.WebP.SimpleEncoder simpleEncoder = new Imazen.WebP.SimpleEncoder();
+           
             System.Drawing.Image image = System.Drawing.Image.FromFile(tempFilePath);
             using (var bmp = new System.Drawing.Bitmap(image.Width, image.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
             {
@@ -37,7 +37,7 @@ namespace NBeeNET.Mjolnir.Storage.Image.Jobs
                 //转码并保存文件
                 using (var fs = System.IO.File.Create(webPPath))
                 {
-                    simpleEncoder.Encode(bmp, fs, 100);
+                    Imazen.WebP.SimpleEncoder.Encode(bmp, fs, 100);
                 }
             }
 
