eed1ffc64db7740bc40166dcc76211a2c449bfce
zhulige <zhulige@qq.com>
2019-05-07 17:04:06 +0800

    PrintPDFJob.cs

---
+++ src/NBeeNET.Mjolnir.Storage.Office/Jobs/PrintPDFJob.cs
@@ -0,0 +1,23 @@
+﻿using NBeeNET.Mjolnir.Storage.Core.Models;
+using Spire.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NBeeNET.Mjolnir.Storage.Office.Jobs
+{
+    public class PrintPDFJob
+    {
+        public JsonFileValues Run(string tempFilePath, JsonFileValues job)
+        {
+            PdfDocument doc = new PdfDocument();
+
+            //Load file
+            doc.LoadFromFile(@"D:\PrintPdfDocument.pdf");
+            
+            doc.Print();
+           
+            return null;
+        }
+    }
+}
