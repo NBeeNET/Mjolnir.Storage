dab64d75e7dc1aea15b1d9da0a5af71c702e66ed
liuxiangyu <liuxyxh@outlook.com>
2019-05-10 15:48:14 +0800

    更新

--- src/NBeeNET.Mjolnir.Storage.Audio/Common/AudioValidationClass.cs
+++ src/NBeeNET.Mjolnir.Storage.Audio/Common/AudioValidationClass.cs
@@ -40,44 +40,17 @@ namespace NBeeNET.Mjolnir.Storage.Audio
         }
         private enum AudioFormat
         {
-            bmp,
-            jpeg,
-            gif,
-            tiff,
-            png,
+            wav,
+            mp3,
+            ogg,
+            acc,
+            webm,
             unknown
         }
 
         private static AudioFormat GetAudioFormat(byte[] bytes)
         {
-            var bmp = Encoding.ASCII.GetBytes("BM");     // BMP
-            var gif = Encoding.ASCII.GetBytes("GIF");    // GIF
-            var png = new byte[] { 137, 80, 78, 71 };    // PNG
-            var tiff = new byte[] { 73, 73, 42 };         // TIFF
-            var tiff2 = new byte[] { 77, 77, 42 };         // TIFF
-            var jpeg = new byte[] { 255, 216, 255, 224 }; // jpeg
-            var jpeg2 = new byte[] { 255, 216, 255, 225 }; // jpeg canon
 
-            if (bmp.SequenceEqual(bytes.Take(bmp.Length)))
-                return AudioFormat.bmp;
-
-            if (gif.SequenceEqual(bytes.Take(gif.Length)))
-                return AudioFormat.gif;
-
-            if (png.SequenceEqual(bytes.Take(png.Length)))
-                return AudioFormat.png;
-
-            if (tiff.SequenceEqual(bytes.Take(tiff.Length)))
-                return AudioFormat.tiff;
-
-            if (tiff2.SequenceEqual(bytes.Take(tiff2.Length)))
-                return AudioFormat.tiff;
-
-            if (jpeg.SequenceEqual(bytes.Take(jpeg.Length)))
-                return AudioFormat.jpeg;
-
-            if (jpeg2.SequenceEqual(bytes.Take(jpeg2.Length)))
-                return AudioFormat.jpeg;
 
             return AudioFormat.unknown;
         }
--- src/NBeeNET.Mjolnir.Storage.Video/Common/VideoValidationClass.cs
+++ src/NBeeNET.Mjolnir.Storage.Video/Common/VideoValidationClass.cs
@@ -40,44 +40,14 @@ namespace NBeeNET.Mjolnir.Storage.Video
         }
         private enum VideoFormat
         {
-            bmp,
-            jpeg,
-            gif,
-            tiff,
-            png,
+            ogg,
+            mpeg4,
+            webm,
             unknown
         }
 
         private static VideoFormat GetVideoFormat(byte[] bytes)
         {
-            var bmp = Encoding.ASCII.GetBytes("BM");     // BMP
-            var gif = Encoding.ASCII.GetBytes("GIF");    // GIF
-            var png = new byte[] { 137, 80, 78, 71 };    // PNG
-            var tiff = new byte[] { 73, 73, 42 };         // TIFF
-            var tiff2 = new byte[] { 77, 77, 42 };         // TIFF
-            var jpeg = new byte[] { 255, 216, 255, 224 }; // jpeg
-            var jpeg2 = new byte[] { 255, 216, 255, 225 }; // jpeg canon
-
-            if (bmp.SequenceEqual(bytes.Take(bmp.Length)))
-                return VideoFormat.bmp;
-
-            if (gif.SequenceEqual(bytes.Take(gif.Length)))
-                return VideoFormat.gif;
-
-            if (png.SequenceEqual(bytes.Take(png.Length)))
-                return VideoFormat.png;
-
-            if (tiff.SequenceEqual(bytes.Take(tiff.Length)))
-                return VideoFormat.tiff;
-
-            if (tiff2.SequenceEqual(bytes.Take(tiff2.Length)))
-                return VideoFormat.tiff;
-
-            if (jpeg.SequenceEqual(bytes.Take(jpeg.Length)))
-                return VideoFormat.jpeg;
-
-            if (jpeg2.SequenceEqual(bytes.Take(jpeg2.Length)))
-                return VideoFormat.jpeg;
 
             return VideoFormat.unknown;
         }
