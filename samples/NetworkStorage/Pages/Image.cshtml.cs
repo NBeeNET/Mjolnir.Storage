5dbf6a25c0bda36b11a75dfa5a343129a301fe59
zhulige <zhulige@qq.com>
2019-05-15 10:33:47 +0800

    NetworkStorage.csproj

---
+++ samples/NetworkStorage/Pages/Image.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NetworkStorage.Pages
+{
+    public class ImageModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
