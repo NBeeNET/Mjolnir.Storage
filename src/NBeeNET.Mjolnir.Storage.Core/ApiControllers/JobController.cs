13e8ecf63f53115ed0e3808dfaaebaa875bc046f
zhulige <zhulige@qq.com>
2019-05-16 15:43:47 +0800

    Office

---
+++ src/NBeeNET.Mjolnir.Storage.Core/ApiControllers/JobController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NBeeNET.Mjolnir.Storage.Core.ApiControllers
+{
+    [Route("/StorageApi/[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("成功！");
+        }
+        
+    }
+}
