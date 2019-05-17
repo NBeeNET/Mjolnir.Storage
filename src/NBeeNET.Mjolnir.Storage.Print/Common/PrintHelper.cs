6c337960ba377b61ae7723fe322a272aa83f3080
蒋琦 <948856247@qq.com>
2019-05-17 15:15:08 +0800

    print api

--- src/NBeeNET.Mjolnir.Storage.Print/Common/PrintHelper.cs
+++ src/NBeeNET.Mjolnir.Storage.Print/Common/PrintHelper.cs
@@ -4,60 +4,64 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NBeeNET.Mjolnir.Storage.Print.Office
+namespace NBeeNET.Mjolnir.Storage.Print.Common
 {
-    public class Word
+    /// <summary>
+    /// 打印helper
+    /// </summary>
+    public class PrintHelper
     {
         /// <summary>
-        /// 同步打印
+        /// excel打印
         /// </summary>
         /// <param name="path"></param>
-        /// <param name="msg"></param>
-        /// <param name="printerName">打印机名称</param>
-        public static void Print(string path, out string msg,object outFileName = null, string printerName = null)
+        /// <param name="printerName"></param>
+        /// <param name="jobModel"></param>
+        public static async void ExcelPrintAsync(string path, string outFileName = null, string printerName = null, PrintJobModel jobModel = null)
         {
-            //打印的代码如下：
-            Microsoft.Office.Interop.Word.Application app = null;
-            Microsoft.Office.Interop.Word.Document doc = null;
-            object missing = System.Reflection.Missing.Value;
-            object templateFile = path;
-            try
+            await Task.Run(() =>
             {
-                app = new Microsoft.Office.Interop.Word.ApplicationClass();
-                if (!string.IsNullOrEmpty(printerName))
-                    app.ActivePrinter = printerName;
-                doc = app.Documents.OpenNoRepairDialog(ref templateFile, ref missing, ref missing, ref missing);
-                //打印
-                if(string.IsNullOrEmpty(outFileName?.ToString()))
-                    outFileName = path.ToLower().Replace(".docx", ".pdf").Replace(".doc", ".pdf");
-                doc.PrintOut(OutputFileName: ref outFileName);
-                msg = "打印完成";
-                //doc.PrintOut(ref missing, ref missing, ref missing, ref missing,
-                //     ref missing, ref missing, ref missing, ref missing, ref missing,
-                //     ref missing, ref missing, ref missing, ref missing, ref missing,
-                //     ref missing, ref missing, ref missing, ref missing);
-            }
-            catch (Exception exp)
-            {
-                msg = "打印异常";
-                //throw exp;
-            }
-            //销毁word进程
-            finally
-            {
-                object saveChange = Microsoft.Office.Interop.Word.WdSaveOptions.wdDoNotSaveChanges;
-                if (doc != null)
-                    doc.Close(ref saveChange, ref missing, ref missing);
-                if (app != null)
-                    app.Quit(ref missing, ref missing, ref missing);
-            }
+                if (jobModel == null)
+                    jobModel = new PrintJobModel();
+                Microsoft.Office.Interop.Excel.Application app = null;
+                Microsoft.Office.Interop.Excel.Workbook worksBook = null;
+                string templateFile = path;
+                try
+                {
+                    app = new Microsoft.Office.Interop.Excel.Application(); //声明一个应用程序类实例
+                    //ExcelApp.DefaultFilePath = ""; //默认文件路径导出excel的路径还是在参数strFileName里设置
+                    //ExcelApp.DisplayAlerts = true;
+                    //ExcelApp.SheetsInNewWorkbook = 1;///返回或设置 Microsoft Excel 自动插入到新工作簿中的工作表数目。
+                    worksBook = app.Workbooks.Open(templateFile); //创建一个新工作簿
+                    Microsoft.Office.Interop.Excel.Worksheet workSheet = (Microsoft.Office.Interop.Excel.Worksheet)worksBook.Worksheets[1]; //在工作簿中得到sheet。
+                    if (string.IsNullOrEmpty(outFileName))
+                        outFileName = path.Replace(".xlsx", ".pdf").Replace(".xls", ".pdf");
+                    workSheet.PrintOutEx(PrToFileName: outFileName, ActivePrinter: printerName);
+                    jobModel.JobStatus = "打印完成";
+                }
+                catch (Exception exp)
+                {
+                    jobModel.JobStatus = "打印异常";
+                    throw exp;
+                }
+                //销毁excel进程
+                finally
+                {
+                    object saveChange = Microsoft.Office.Interop.Word.WdSaveOptions.wdDoNotSaveChanges;
+                    if (worksBook != null)
+                        worksBook.Close();
+                    if (app != null)
+                        app.Quit();
+                }
+            });
         }
+
         /// <summary>
-        /// 异步打印
+        /// word打印
         /// </summary>
         /// <param name="path"></param>
         /// <param name="jobModel"></param>
-        public static async void PrintAsync(string path, object outFileName = null, string printerName = null, PrintJobModel jobModel = null)
+        public static async void WordPrintAsync(string path, object outFileName = null, string printerName = null, PrintJobModel jobModel = null)
         {
             await Task.Run(() =>
             {
@@ -100,7 +104,5 @@ namespace NBeeNET.Mjolnir.Storage.Print.Office
                 }
             });
         }
-
-
     }
 }
---
+++ src/NBeeNET.Mjolnir.Storage.Print/Serivces/PrintHandleService.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Http;
+using NBeeNET.Mjolnir.Storage.Print.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NBeeNET.Mjolnir.Storage.Print.Serivces
+{
+    /// <summary>
+    /// Office处理
+    /// </summary>
+    public class PrintHandleService
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        private PrintHandleService()
+        {
+        }
+        public static PrintHandleService GetResource()
+        {
+            if (_printHandleService == null)
+                _printHandleService = new PrintHandleService();
+            return _printHandleService;
+        }
+
+        private static PrintHandleService _printHandleService = null;
+        /// <summary>
+        /// 打印job状态集合
+        /// </summary>
+        public List<PrintJobModel> printJobModels = new List<PrintJobModel>();
+        /// <summary>
+        /// 根据id获取指定job状态
+        /// </summary>
+        /// <param name="id"></param>
+        public PrintJobModel GetJobById(string id)
+        {
+            UpdateJobsFromLocal();
+            return printJobModels.Find(obj => obj.Document.Contains(id));
+        }
+        /// <summary>
+        /// 更新或添加jobList(远端用)
+        /// </summary>
+        public void UpdateJobsFromRemote(List<PrintJobModel> list)
+        {
+            lock (printJobModels)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    PrintJobModel jobModel = printJobModels.Find(obj => list[i].Document.Contains(obj.Document));
+                    if (jobModel == null)
+                        printJobModels.Add(list[i]);
+                    else
+                    {
+                        jobModel.CopyFrom(list[i]);
+                        jobModel.DataFrom = "远端";
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// 删除已完成的job(远端用)
+        /// </summary>
+        /// <param name="id"></param>
+        public void DeleteJobFromRemote(string ids)
+        {
+            List<string> idList = ids.Split(',').ToList();
+            lock (printJobModels)
+            {
+                for (int i = 0; i < idList.Count; i++)
+                {
+                    PrintJobModel data = printJobModels.Find(obj => obj.Document.Contains(idList[i]));
+                    if (data != null)
+                        printJobModels.Remove(data);
+                }
+            }
+        }
+        /// <summary>
+        /// 更新.添加.删除jobList(本地用)
+        /// </summary>
+        public void UpdateJobsFromLocal(string printerName = null)
+        {
+            lock (printJobModels)
+            {
+                try
+                {
+                    if (string.IsNullOrEmpty(printerName))
+                    {
+                        Print.Common.PrinterHelper.Printer printer = Print.Common.PrinterHelper.GetDefaultPrinter();
+                        printerName = printer.Name;
+                    }
+                    List<PrintJobModel> _List = Print.Common.PrinterHelper.GetPrintJobs(printerName);
+                    foreach (PrintJobModel printJobModel in _List)
+                    {
+                        var data = printJobModels.Find(obj => printJobModel.Document.Contains(obj.Document));
+                        if (data != null)
+                        {
+                            //更新
+                            data.CopyFrom(printJobModel);
+                            data.DataFrom = "本地";
+                        }
+                        else
+                        {
+                            //添加
+                            printJobModels.Add(printJobModel);
+                            printJobModel.DataFrom = "本地";
+                        }
+                    }
+                    //当前打印机缓存的job
+                    List<PrintJobModel> printerJob = printJobModels.FindAll(obj => obj.DriverName == printerName);
+                    foreach (PrintJobModel printJobModel in printerJob)
+                    {
+                        var data = _List.Find(obj => obj.Document.Contains(printJobModel.Document));
+                        if (data == null)
+                        {
+                            //删除
+                            printJobModels.Remove(printJobModel);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+    }
+}
